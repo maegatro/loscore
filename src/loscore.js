0e58d21c204fad27cdfeb5f97d31cc50a80ceefc
Zowie <minjiayi22@gmail.com>
2020-02-19 21:53:14 +0900

    Wrote identity method

--- src/loscore.js
+++ src/loscore.js
@@ -1,10 +1,10 @@
 (() => {
-  'use strict';
-  
+  "use strict";
+
   window._ = {};
 
   _.identity = (val) => {
-    // YOUR CODE HERE
+    return val;
   };
 
   _.add = (x, y) => {
@@ -14,35 +14,78 @@
   /**
   | ARRAYS
   |~~~~~~~~~~
-  **/ 
+  **/
 
   _.head = (array) => {
     return array[0];
   };
 
   _.tail = (array) => {
-    // YOUR CODE HERE
+    let result = [];
+    for (let i = 1; i < array.length; i++) {
+      result.push(array[i]);
+    }
+    return result;
   };
 
   _.take = (array, n) => {
-    // YOUR CODE HERE
+    if (n == undefined) {
+      return array[0];
+    } else {
+      let result = [];
+      for (let i = 0; i < n; i++) {
+        if (array[i] != undefined) {
+          result.push(array[i]);
+        }
+      }
+      return result;
+    }
   };
 
   _.takeRight = (array, n) => {
-    // YOUR CODE HERE
+    if (n == undefined) {
+      return array[array.length - 1];
+    } else {
+      let result = [];
+      for (let i = array.length - n; i < array.length; i++) {
+        if (array[i] != undefined) {
+          result.push(array[i]);
+        }
+      }
+      return result;
+    }
   };
 
   _.uniq = (array) => {
-    // YOUR CODE HERE
+    let copy = [];
+    for (const i of array) {
+      copy.push(i);
+    }
+    for (let i = 0; i < array.length; i++) {
+      for (let j = i + 1; j < copy.length; j++) {
+        if (array[i] == copy[j]) {
+          copy[j] = "Duplicate";
+        }
+      }
+    }
+    return copy.filter((ele) => ele != "Duplicate");
   };
 
   /**
   | COLLECTIONS
   |~~~~~~~~~~
-  **/ 
+  **/
 
   _.size = (collection) => {
-    // YOUR CODE HERE
+    if (typeof collection == "string") {
+      return collection.length;
+    } else if (typeof collection == "object") {
+      if (Array.isArray(collection)) {
+        return collection.length;
+      } else {
+        return Object.keys(collection).length;
+      }
+    }
   };
 
   _.indexOf = (array, target) => {
@@ -71,19 +114,22 @@
     });
   };
 
-  _.reduce = (collection, iterator, accumulator) => {
-  };
+  _.reduce = (collection, iterator, accumulator) => {};
 
   _.contains = (collection, target) => {
-    return _.reduce(collection, (wasFound, item) => {
-      if (wasFound) {
-        return true;
-      }
-      return item === target;
-    }, false);
+    return _.reduce(
+      collection,
+      (wasFound, item) => {
+        if (wasFound) {
+          return true;
+        }
+        return item === target;
+      },
+      false
+    );
   };
 
-  _.every = function (/* Your Arguments Here*/) {
+  _.every = function(/* Your Arguments Here*/) {
     // YOUR CODE HERE
   };
 
@@ -92,7 +138,7 @@
   |~~~~~~~~~~
   **/
 
-  _.extend = function (obj) {
+  _.extend = function(obj) {
     // YOUR CODE HERE
   };
 
@@ -101,15 +147,15 @@
   |~~~~~~~~~~
   **/
 
-  _.once = function (func) {
+  _.once = function(func) {
     // YOUR CODE HERE
   };
 
-  _.memoize = function (func) {
+  _.memoize = function(func) {
     // YOUR CODE HERE
   };
-  
-  _.invoke = function (collection, functionOrKey) {
+
+  _.invoke = function(collection, functionOrKey) {
     // YOUR CODE HERE
   };
 
@@ -118,24 +164,23 @@
   |~~~~~~~~~~~~~
   **/
 
-  _.sortBy = function (/* Your Arguments Here */) {
+  _.sortBy = function(/* Your Arguments Here */) {
     // YOUR CODE HERE
   };
 
-  _.zip = function (/* Your Arguments Here */) {
+  _.zip = function(/* Your Arguments Here */) {
     // YOUR CODE HERE
   };
 
-  _.delay = function (/* Your Arguments Here */) {
+  _.delay = function(/* Your Arguments Here */) {
     // YOUR CODE HERE
   };
 
-  _.defaults = function (/* Your Arguments Here */) {
+  _.defaults = function(/* Your Arguments Here */) {
     // YOUR CODE HERE
   };
 
-  _.throttle = function (/* Your Arguments Here */) {
+  _.throttle = function(/* Your Arguments Here */) {
     // YOUR CODE HERE
   };
 })();
-
