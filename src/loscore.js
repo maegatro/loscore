6521d7c4cbf65a260614d29618c578563478b879
Saggitarie <saggitarie@gmail.com>
2020-02-23 16:22:00 +0900

    Refactor every method

--- src/loscore.js
+++ src/loscore.js
@@ -224,14 +224,7 @@
   };
 
   _.every = function (collection, test) {
-    // YOUR CODE HERE
-    console.log("-------------------------------");
-    console.log(`collection is passed as first outer input: ${collection}`);
-    console.log(`test is passed as second outer input: ${test}`);
-
     let result = true;
-
-    // let accumulator = true;
 
     if(test === undefined){
       for(let el of collection){
@@ -241,27 +234,14 @@
         }
       }
     } else {
-    // Check if all elements passes the tests
-    result =  _.reduce(collection, (doesPass, item) => {
-      // IF All Items pass through tests
-      // console.log(`Doespass is ???: ${doesPass}`);
-      if(!doesPass){
-        return false;
+      result =  _.reduce(collection, (doesPass, item) => {
+        if(!doesPass){
+          return false;
+        }
+        return test(item);
+      }, true);
       }
-      return test(item);
-      // console.log(`Outcome of the invocation is: ${test(item)}`);
-        //Return true
-      // Else Some Items or no items pass the tests
-        //Return false
-      // console.log(`Iterator data type: ${Object.prototype.toString.call(val)}`);
-      // console.log(`Value: ${val}`);
-    }, true);
-    }
-
     return result;
-
-
-    console.log("-------------------------------");
   };
 
   /**
