f997255f845c95adb4023578d775f7d8a20fd760
Carlos Salazar <carlosiusalazar@gmail.com>
2020-02-23 13:11:23 +0900

    Tried one last ditch attempt. Made a new file, copied the code as text only, beautified and refactored looking for empty spaces or anything else that may be cause the problem but sadly no solution was found yet.

--- src/loscore.js
+++ src/loscore.js
@@ -1,10 +1,8 @@
 (() => {
-  'use strict';
-  
-  window._ = {};
+  'use strict'; window._ = {};
 
   _.identity = (val) => {
-    // YOUR CODE HERE
+    // YOUR CODE HERE 
     return val;
   };
 
@@ -12,152 +10,136 @@
     return x + y;
   };
 
-  /**
-  | ARRAYS
-  |~~~~~~~~~~
-  **/ 
+  /** | ARRAYS |~~~~~~~~~~ **/
 
   _.head = (array) => {
     return array[0];
   };
 
   _.tail = (array) => {
-    // YOUR CODE HERE
+    // YOUR CODE HERE 
     let returnArray = [];
-    for(let i=1; i<=array.length-1;i++){
+    for (let i = 1; i <= array.length - 1; i++) {
       returnArray.push(array[i]);
     }
     return returnArray;
   };
 
   _.take = (array, n) => {
-    // YOUR CODE HERE
+    // YOUR CODE HERE 
     let returnArray = [];
-
-    if (n === 0){
-        return returnArray;
+    if (n === 0) {
+      return returnArray;
     }
-
-    if (!n){
-        returnArray.push(array[0]);
-        return returnArray;
+    if (!n) {
+      returnArray.push(array[0]);
+      return returnArray;
     }
-
-    for(let i = 0; i <= n-1; i++){
-        if (typeof array[i] !== "undefined"){
-            returnArray.push(array[i])
-        }
-    }
-    return returnArray;
+    for (let i = 0; i <= n - 1; i++) {
+      if (typeof array[i] !== "undefined") {
+        returnArray.push(array[i])
+      }
+    } return returnArray;
   };
 
   _.takeRight = (array, n) => {
-    // YOUR CODE HERE
+    // YOUR CODE HERE 
     let returnArray = [];
-
-    if (n === 0){
-        return returnArray;
+    if (n === 0) {
+      return returnArray;
     }
-
-    if (!n){
-        returnArray.push(array[array.length-1]);
-        return returnArray;
+    if (!n) {
+      returnArray.push(array[array.length - 1]);
+      return returnArray;
     }
-
-    for(let i = (array.length - n); i <= n; i++){
-        if (typeof array[i] !== "undefined"){
-            returnArray.push(array[i])
-        }
-    }
-    return returnArray; 
+    for (let i = (array.length - n); i <= n; i++) {
+      if (typeof array[i] !== "undefined") {
+        returnArray.push(array[i])
+      }
+    } return returnArray;
   };
 
   _.uniq = (array) => {
-    // YOUR CODE HERE
+    // YOUR CODE HERE 
     let returnArray = [];
     let tempValue = undefined;
     let alreadyExists = false;
-
-    for(let i = 0; i < array.length; i++){
-        tempValue = array[i];
-        for(let j = 0; j<= returnArray.length; j++){
-            if(returnArray[j] === tempValue){
-                alreadyExists = true;
-            }
+    for (let i = 0; i < array.length; i++) {
+      tempValue = array[i];
+      for (let j = 0; j <= returnArray.length; j++) {
+        if (returnArray[j] === tempValue) {
+          alreadyExists = true;
         }
-        if (alreadyExists === false){
-            returnArray.push(tempValue);
-        }
-        alreadyExists = false;
-        tempValue = undefined;
+      }
+      if (alreadyExists === false) {
+        returnArray.push(tempValue);
+      }
+      alreadyExists = false;
+      tempValue = undefined;
     }
     return returnArray;
   };
 
-  /**
-  | COLLECTIONS
-  |~~~~~~~~~~
-  **/ 
-
+  /** | COLLECTIONS |~~~~~~~~~~ **/
   _.size = (collection) => {
-    // YOUR CODE HERE
-    if (Array.isArray(collection)){
+    // YOUR CODE HERE 
+    if (Array.isArray(collection)) {
       return collection.length
     } else if (typeof collection === 'object' && collection.constructor === Object) {
       return Object.keys(collection).length;
-    } else if (typeof collection === "string"){
+    } else if (typeof collection === "string") {
       return collection.length;
     }
   };
 
   _.indexOf = (array, target) => {
-    // YOUR CODE HERE
+    // YOUR CODE HERE 
     let exists = [];
-    _.each(array,function(value,i){
-      // console.log("array",array)
-      // console.log("value",value)
-      // console.log("i",i)
-      // console.log("target",target)
-      if (value === target){
-        //console.log("Found!", i)
+    _.each(array, function (value, i) {
+      // console.log("array",array) 
+      // console.log("value",value) 
+      // console.log("i",i) 
+      // console.log("target",target) 
+      if (value === target) {
+        //console.log("Found!", i) 
         exists.push(i)
       }
     })
-    if(exists.length === 0){
+    if (exists.length === 0) {
       return -1;
     } else {
-    return exists[0];
+      return exists[0];
     }
   };
 
   _.each = (collection, iteratee) => {
-    // YOUR CODE HERE
+    // YOUR CODE HERE 
     let type = typeof collection;
-    if (collection instanceof Array){
-      for (let i = 0; i < collection.length; i++){
+    if (collection instanceof Array) {
+      for (let i = 0; i < collection.length; i++) {
         iteratee(collection[i], i, collection);
-      } 
-      } else if (type === "object") {
-        for (let item in collection) {
-          iteratee(collection[item], item, collection);
-        }
       }
+    } else if (type === "object") {
+      for (let item in collection) {
+        iteratee(collection[item], item, collection);
+      }
+    }
   };
 
   _.map = (collection, iteratee) => {
-    // YOUR CODE HERE
+    // YOUR CODE HERE 
     let resultArray = [];
-    _.each(collection,function(value){
+    _.each(collection, function (value) {
       resultArray.push(iteratee(value))
     })
     return resultArray;
   };
 
   _.filter = (collection, test) => {
-    // YOUR CODE HERE
+    // YOUR CODE HERE 
     let resultArray = [];
-    _.each(collection,function(value){
-      if(test(value)){
+    _.each(collection, function (value) {
+      if (test(value)) {
         resultArray.push(value)
       }
     })
@@ -165,10 +147,10 @@
   };
 
   _.reject = (collection, test) => {
-    // YOUR CODE HERE
+    // YOUR CODE HERE 
     let resultArray = [];
-    _.filter(collection,function(value){
-      if(!test(value)){
+    _.filter(collection, function (value) {
+      if (!test(value)) {
         resultArray.push(value)
       }
     })
@@ -177,15 +159,14 @@
 
   _.pluck = (collection, key) => {
     let resultArray = [];
-    for(let object of collection){
-      for(let id in object){
-        if(id === key){
+    for (let object of collection) {
+      for (let id in object) {
+        if (id === key) {
           resultArray.push(object[id])
         }
       }
-    }
-    //console.log("resultArray is", resultArray)
-    return resultArray;    
+    } //console.log("resultArray is", resultArray) 
+    return resultArray;
   };
 
   _.reduce = (collection, iterator, accumulator) => {
@@ -194,10 +175,10 @@
         collection = collection[0];
         accumulator = iterator(val, 0);
       } else {
-        //console.log(accumulator)
+        //console.log(accumulator) 
         accumulator = iterator(accumulator, val);
       }
-      })
+    })
     return accumulator;
   };
 
@@ -210,100 +191,93 @@
     }, false);
   };
 
-  _.every = function (collection,test) {
-    // YOUR CODE HERE
-    if (test === undefined) { return true; } return _.reduce(collection, (wasPassed, item) => { if (!wasPassed) { return false; } return test(item); }, true);
+  _.every = function (collection, test) {
+    // YOUR CODE HERE 
+    if (test === undefined) {
+      return true;
+    }
+    return _.reduce(collection, (wasPassed, item) => {
+      if (!wasPassed) {
+        return false;
+      }
+      return test(item);
+    }, true);
   };
 
-  /**
-  | OBJECTS
-  |~~~~~~~~~~
-  **/
-
+  /** | OBJECTS |~~~~~~~~~~ **/
   _.extend = function (...obj) {
-    // YOUR CODE HERE
-    for(let i = 0; i < obj.length-1; i++){
-      _.each(obj[i+1],(value,key)=>{
+    // YOUR CODE HERE 
+    for (let i = 0; i < obj.length - 1; i++) {
+      _.each(obj[i + 1], (value, key) => {
         obj[0][key] = value;
       });
     }
     return obj[0];
   };
 
-  /**
-  | FUNCTIONS
-  |~~~~~~~~~~
-  **/
-
+  /** | FUNCTIONS |~~~~~~~~~~ **/
   _.once = function (func) {
-    // YOUR CODE HERE
+    // YOUR CODE HERE 
     let alreadyCalled = false;
     let result;
-    return function() {
-        if (!alreadyCalled) {
-            result = func.apply(this, arguments);
-            //console.log(result)
-            alreadyCalled = true;
-        }
-        return result;
+    return function () {
+      if (!alreadyCalled) {
+        result = func.apply(this, arguments);
+        //console.log(result) 
+        alreadyCalled = true;
+      }
+      return result;
     };
   };
 
   _.memoize = function (func) {
-    // YOUR CODE HERE
+    // YOUR CODE HERE 
     let cache = {};
-    return function(){
+    return function () {
       let key = JSON.stringify(arguments);
-      if (cache[key]){
-        //console.log(cache)
+      if (cache[key]) {
+        //console.log(cache) 
         return cache[key];
-      }
-      else{
+      } else {
         let val = func.apply(null, arguments);
-        //console.log(val)
+        //console.log(val) 
         cache[key] = val;
-        return val; 
+        return val;
       }
-  }
+    }
   };
-  
+
   _.invoke = function (collection, functionOrKey) {
-    // YOUR CODE HERE
-    let resultArr =[];
-    //console.log(resultArr)
-    if (typeof functionOrKey !== "function"){
-      resultArr = _.map(collection, (i)=> {
+    // YOUR CODE HERE 
+    let resultArr = [];
+    //console.log(resultArr) 
+    if (typeof functionOrKey !== "function") {
+      resultArr = _.map(collection, (i) => {
         return i[functionOrKey].apply(i)
-    })
-  } else if (typeof functionOrKey === "function") {
-      resultArr = _.map(collection, (i)=> {
-      return functionOrKey.apply(i)
-    })}
+      })
+    } else if (typeof functionOrKey === "function") {
+      resultArr = _.map(collection, (i) => {
+        return functionOrKey.apply(i)
+      })
+    }
     return resultArr;
   };
 
-  /**
-  | ADVANCED REQUIREMENTS
-  |~~~~~~~~~~~~~
-  **/
+  /** | ADVANCED REQUIREMENTS |~~~~~~~~~~~~~ **/
 
   _.sortBy = function (/* Your Arguments Here */) {
-    // YOUR CODE HERE
+    // YOUR CODE HERE 
   };
-
   _.zip = function (/* Your Arguments Here */) {
-    // YOUR CODE HERE
+    // YOUR CODE HERE 
   };
-
   _.delay = function (/* Your Arguments Here */) {
-    // YOUR CODE HERE
+    // YOUR CODE HERE 
   };
-
   _.defaults = function (/* Your Arguments Here */) {
-    // YOUR CODE HERE
+    // YOUR CODE HERE 
   };
-
   _.throttle = function (/* Your Arguments Here */) {
-    // YOUR CODE HERE
+    // YOUR CODE HERE 
   };
 })();
